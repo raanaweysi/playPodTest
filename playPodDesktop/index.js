fec8371fb87b053cd1e243949265e715942850ef
Rana Weysi <ranaweysi@gmail.com>
2023-06-17 12:43:01 +0330

    fix: sorting project folders

--- playPodDesktop/index.js
+++
@@ -1,6 +0,0 @@
-let custume_carousel = document.querySelector('.navbar');
-
-custume_carousel.addEventListener("click", (event)=>{
-  event.preventDefault();
-  event.target.classList.add('.active');
-});
